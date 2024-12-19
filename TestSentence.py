3d1b20d00cbb81255f178987c2e045ab2d356d1f
Kirill <krvchnkkrll@gmail.com>
2024-12-19 22:56:39 +0300

    TestSentence

---
+++ TestSentence.py
@@ -0,0 +1,22 @@
+import re
+
+queries = [
+    "Cколько часов челавеку нужно спать в утки чтобы нормально себя чуствовать и быть роботоспособным?",
+    "Чтобы нормально себя чувствовать и быть работоспособным, cколько часов человеку нужно спать в сутки?",
+    "Сколько часов человеку нужно спать чтобы оставаться работоспособным?"
+
+]
+
+answer = "Сколько часов человеку нужно спать в сутки, чтобы нормально себя чувствовать и быть работоспособным?"
+
+
+def clean_sentence(sentence):
+    return re.sub(r'[^\w\s]', '', sentence).lower()
+
+
+cleaned_queries = []
+
+cleaned_answer = clean_sentence(answer)
+
+for query in queries:
+    cleaned_queries.append(clean_sentence(query))
